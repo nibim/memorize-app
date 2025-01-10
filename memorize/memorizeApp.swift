ef49b72e0ca6ea254063106b1b3c2bb61327da65
nibim <nibm@kth.se>
2025-01-10 16:56:56 +0100

    Initial Commit

---
+++ memorize/memorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorizeApp.swift
+//  memorize
+//
+//  Created by Nima Beig Mohammadi on 10.01.25.
+//
+
+import SwiftUI
+
+@main
+struct memorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ memorizeTests/memorizeTests.swift
@@ -0,0 +1,16 @@
+//
+//  memorizeTests.swift
+//  memorizeTests
+//
+//  Created by Nima Beig Mohammadi on 10.01.25.
+//
+
+import Testing
+
+struct memorizeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
