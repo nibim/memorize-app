8da54b4904760b383694c249c2340d21e8626cdf
nibim <nibm@kth.se>
2025-01-12 18:55:05 +0100

    simple array representation and features

--- memorize/ContentView.swift
+++ memorize/ContentView.swift
@@ -8,14 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    let emojis: [String] = ["🥳", "👻", "🥺", "💀", "🤯"]
+    @State var cardCount = 4
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Nima, world!")
+            ForEach(0..<cardCount, id: \.self) { index in
+                CardView(content: emojis[index])
+            }
+            
         }
         .padding()
+    }
+}
+
+struct CardView: View {
+    
+    let content: String
+    @State var isFaceUp: Bool = true
+    let base = RoundedRectangle(cornerRadius: 12)
+    
+    
+    var body: some View {
+        ZStack {
+            Group{
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
+                Text(content).font(.largeTitle)
+                    
+            }
+        }
     }
 }
 
