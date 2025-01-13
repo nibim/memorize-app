719e7a9e44a264174384684be3a97e5048b24546
nibim <nibm@kth.se>
2025-01-13 01:29:15 +0100

    Card Count adjuster half is implemented

--- memorize/ContentView.swift
+++ memorize/ContentView.swift
@@ -9,17 +9,45 @@ import SwiftUI
 
 struct ContentView: View {
     let emojis: [String] = ["🥳", "👻", "🥺", "💀", "🤯"]
-    @State var cardCount = 4
+    @State var cardCount = 5
     var body: some View {
         VStack {
-            ForEach(0..<cardCount, id: \.self) { index in
-                CardView(content: emojis[index])
-            }
-            
+            ScrollView{cards}
+            cardCountAdjusters
         }
         .padding()
     }
+    
+    var cards : some View {                //cards is a computed property which reperesents SwiftUI view
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]){
+            ForEach(0..<cardCount, id: \.self) { index in
+                CardView(content: emojis[index])
+                    .aspectRatio(2/3, contentMode: .fit)
+            }
+        }
+        .foregroundColor(.orange)
+    }
+    
+    var cardCountAdjusters: some View {
+        HStack {
+            cardCountAdjuster(by: -1, symbol: "rectangle.stack.badge.minus.fill")
+        }
+        .imageScale(.large)
+        .font(.largeTitle)
+    }
+    
+    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
+        Button(action: {
+            cardCount += offset },
+            label: {
+                Image(systemName: symbol)}
+        ) }
+            
+            
 }
+    
+    
+    
 
 struct CardView: View {
     
@@ -34,9 +62,13 @@ struct CardView: View {
                 base.fill(.white)
                 base.strokeBorder(lineWidth: 2)
                 Text(content).font(.largeTitle)
-                    
             }
+            .opacity(isFaceUp ? 1 : 0)
+            base.opacity(isFaceUp ? 0 : 1)
+        }.onTapGesture {
+            isFaceUp.toggle()
         }
+        
     }
 }
 
