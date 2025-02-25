87940af3b48545480a4743f3c649740cac251de5
nibim <nibm@kth.se>
2025-02-25 13:38:01 +0100

    The spacing has been removed and instead , padding is used.

--- memorize/EmojiMemoryGameView.swift
+++ memorize/EmojiMemoryGameView.swift
@@ -18,10 +18,11 @@ struct EmojiMemoryGameView: View {
         .padding()
     }
     var cards : some View {                //cards is a computed property which reperesents SwiftUI view
-        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]){
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 85), spacing: 0)], spacing: 0){
             ForEach(viewModel.cards.indices, id: \.self) { index in
                 CardView(viewModel.cards[index])
                     .aspectRatio(2/3, contentMode: .fit)
+                    .padding(4)
             }
         }
         .foregroundColor(Color.orange)
