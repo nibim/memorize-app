b2364785a46fbeb752ade58be71264d5aa6348f4
nibim <nibm@kth.se>
2025-03-01 15:10:27 +0100

    Fix: Using optional value

--- memorize/MemoryGame.swift
+++ memorize/MemoryGame.swift
@@ -23,17 +23,18 @@ struct MemoryGame<CardContent> where CardContent: Equatable {                //C
     }
     
     mutating func choose (_ card: Card){
-        let chosenIndex = index(of: card)
-        cards[chosenIndex].isFaceUp.toggle()
+        if let chosenIndex = index(of: card){
+            cards[chosenIndex].isFaceUp.toggle()
+        }
     }
     
-    func index(of card: Card) -> Int {
+    private func index(of card: Card) -> Int? {
         for index in cards.indices {
             if cards[index].id == card.id {
                 return index
             }
         }
-        return 0
+        return nil
     }
     
     
