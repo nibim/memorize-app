fe66727dd13fa4e6cbc5cc63306f4100478d1ad3
nibim <nibm@kth.se>
2025-03-04 14:50:41 +0100

    Feature:  view AspectVgrid was coded and used in EmojiMemoryGame

---
+++ AspectVGrid.swift
@@ -0,0 +1,18 @@
+//
+//  AspectVGrid.swift
+//  memorize
+//
+//  Created by Nima Beig Mohammadi on 04.03.25.
+//
+
+import SwiftUI
+
+struct AspectVGrid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AspectVGrid()
+}
