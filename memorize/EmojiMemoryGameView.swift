dbffda544b38d3dbf0158b1e5a3de7b68e6152a1
nibim <nibm@kth.se>
2025-03-05 14:10:57 +0100

    Feature: made a new Swift view called CardView and instead refined the emojiGameView code part

--- memorize/EmojiMemoryGameView.swift
+++ memorize/EmojiMemoryGameView.swift
@@ -34,35 +34,6 @@ struct EmojiMemoryGameView: View {
             .foregroundColor(Color.orange)
     }
 }
-    
-    
-    
-
-struct CardView: View {
-    let card: MemoryGame<String>.Card // it is here that will bridge view to model 🌉
-    
-    init(_ card: MemoryGame<String>.Card) {
-        self.card = card
-    }
-    
-    var body: some View {
-        ZStack {
-            let base = RoundedRectangle(cornerRadius: 12)
-            Group{
-                base.fill(.white)
-                base.strokeBorder(lineWidth: 2)
-                Text(card.content)
-                .font(.system(size: 200))
-                .minimumScaleFactor(0.01)
-                .aspectRatio(1, contentMode: .fit)
-            }
-                .opacity(card.isFaceUp ? 1 : 0)
-            base.fill()
-                .opacity(card.isFaceUp ? 0 : 1 )
-        }
-        .opacity(card.isFaceUp || !card.isMatched ? 1 : 0)
-    }
-}
 
 #Preview {
     EmojiMemoryGameView(viewModel: EmojiMemoryGame())  // This will fix the error
