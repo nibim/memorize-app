1b63836a1ecda987a9ce808bd3bd5e17d987ec91
nibim <nibm@kth.se>
2025-03-14 14:30:24 +0100

    Feature: FlyingNumber View was implemented totally completely

---
+++ memorize/FlyingNumber.swift
@@ -0,0 +1,18 @@
+//
+//  FlyingNumber.swift
+//  memorize
+//
+//  Created by Nima Beig Mohammadi on 14.03.25.
+//
+
+import SwiftUI
+
+struct FlyingNumber: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FlyingNumber()
+}
